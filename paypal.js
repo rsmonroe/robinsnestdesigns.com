462bc399a8c8d21498e98a2eee1f77c5e80eeeb4
Jon Monroe <berkona@gmail.com>
2019-07-21 09:44:13 -0700

    fixed env

--- paypal.js
+++ paypal.js
@@ -1,12 +1,12 @@
 const paypal = require('@paypal/checkout-server-sdk');
 
 // Creating an environment
-if (!process.env.PAYPAL_CLIENTID || !process.env.PAYPAL_CLIENTSECRET)
-  throw new Error('You must set the environmental variables: PAYPAL_CLIENTID and PAYPAL_CLIENTSECRET before starting server')
+if (!process.env.PAYPAL_CLIENTID || !process.env.PAYPAL_CLIENTSECRET || !process.env.PAYPAL_ENVIRONMENT)
+  throw new Error('You must set the environmental variables: PAYPAL_CLIENTID, PAYPAL_CLIENTSECRET, and PAYPAL_ENVIRONMENT before starting server')
 
 let clientId =  process.env.PAYPAL_CLIENTID
 let clientSecret = process.env.PAYPAL_CLIENTSECRET
-let environment = process.env.NODE_ENV == "production"
+let environment = process.env.PAYPAL_ENVIRONMENT == "live"
   ? new paypal.core.LiveEnvironment(clientId, clientSecret)
   : new paypal.core.SandboxEnvironment(clientId, clientSecret)
 let client = new paypal.core.PayPalHttpClient(environment)
